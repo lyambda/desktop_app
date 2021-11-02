5ed038c511fbfa58cef642e2abce439aa7c0209c
Всеволод <63463168+vsecoder@users.noreply.github.com>
2021-11-02 14:53:35 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from 'electron';
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + "/icon.ico"
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
